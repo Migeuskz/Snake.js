f83d81e605f0f9b57848329b562b0c62b7db625a
Migeuskz <msanchezpapalotzi@gmail.com>
2024-01-09 10:26:22 -0600

    agregando movimiento a snake

--- app.js
+++ app.js
@@ -4,6 +4,23 @@ const ctx = lienzo.getContext('2d');
 // ctx.fillStyle = 'red'; //colorear = Style
 // ctx.fillRect(0,0,20,20); //dibujar = Rect
 
+let posX = 2;
+let posY = 1;
+
+function nextMove(){
+    snake.forEach((bolita, index) => {
+        if (index === 0){
+            bolita.x = posX;
+            bolita.y = posY;
+            } else{
+                bolita.x = bolita.xNext;
+                bolita.y = bolita.yNext;
+                bolita.xNext = snake[index - 1].x;
+                bolita.yNext = snake[index - 1].y;
+        }
+    })
+}
+
 const snake = [];
 snake.push({
     x:2,
@@ -12,7 +29,7 @@ snake.push({
     yNext:1,
     pinta: function(){
         ctx.font = '25px Serif';
-        ctx.fillText('💀', this.x * 20, this.y *20);
+        ctx.fillText('🐺', this.x * 20, this.y *20);
     }
 });
 
@@ -23,7 +40,7 @@ snake.push({
     yNext:1,
     pinta: function(){
         ctx.font = '25px Serif';
-        ctx.fillText('🖤', this.x * 20, this.y *20);
+        ctx.fillText('⚪', this.x * 20, this.y *20);
     }
 });
 
@@ -34,7 +51,7 @@ snake.push({
     yNext:1,
     pinta: function(){
         ctx.font = '25px Serif';
-        ctx.fillText('🖤', this.x * 20, this.y *20);
+        ctx.fillText('⚫', this.x * 20, this.y *20);
     }
 })
 
@@ -47,11 +64,13 @@ const comida = {
     },
     pinta:function(){
         ctx.font = '25px Serif';
-        ctx.fillText('😎', this.x * 20, this.y * 20);
+        ctx.fillText('🦴', this.x * 20, this.y * 20);
     }
 }
  setInterval(() => {
     ctx.fillRect(0,0, 600, 400);
+    nextMove();
+    posX++;
     // comida.aparece();
     // comida.pinta();
     snake.forEach(bolita => bolita.pinta())
