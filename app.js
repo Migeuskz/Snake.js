2d2c1cca8f590bef05a77711899a111a09410701
Migeuskz <msanchezpapalotzi@gmail.com>
2024-01-09 10:13:15 -0600

    dibujando snake

--- app.js
+++ app.js
@@ -1,23 +1,49 @@
 const lienzo = document.querySelector('#lienzo');
 const ctx = lienzo.getContext('2d');
 //(x,y,ancho,alto)
-ctx.fillStyle = 'red'; //colorear = Style
+// ctx.fillStyle = 'red'; //colorear = Style
 // ctx.fillRect(0,0,20,20); //dibujar = Rect
 
-const snake = {
-    x: 0,
-    y: 0,
+const snake = [];
+snake.push({
+    x:2,
+    y:1,
+    xNext:0,
+    yNext:1,
     pinta: function(){
-        ctx.fillRect(this.x, this.y, 20, 20);
+        ctx.font = '25px Serif';
+        ctx.fillText('💀', this.x * 20, this.y *20);
     }
-}
+});
+
+snake.push({
+    x : 1,
+    y : 1,
+    xNext:0,
+    yNext:1,
+    pinta: function(){
+        ctx.font = '25px Serif';
+        ctx.fillText('🖤', this.x * 20, this.y *20);
+    }
+});
+
+snake.push({
+    x : 0,
+    y : 1,
+    xNext:0,
+    yNext:1,
+    pinta: function(){
+        ctx.font = '25px Serif';
+        ctx.fillText('🖤', this.x * 20, this.y *20);
+    }
+})
 
 const comida = {
     x:0,
     y:0,
     aparece: function(){
         this.x= Math.floor(Math.random() * 30) ;
-        this.y= Math.floor(Math.random() * 30);
+        this.y= Math.floor(Math.random() * 20);
     },
     pinta:function(){
         ctx.font = '25px Serif';
@@ -26,7 +52,8 @@ const comida = {
 }
  setInterval(() => {
     ctx.fillRect(0,0, 600, 400);
-    comida.aparece();
-    comida.pinta();
+    // comida.aparece();
+    // comida.pinta();
+    snake.forEach(bolita => bolita.pinta())
  },500);
 snake.pinta();
