d38f687db09b9c4b30eeab3f6f4cb944e572b104
Migeuskz <msanchezpapalotzi@gmail.com>
2024-01-09 09:38:07 -0600

    iniciando proyecto

---
+++ app.js
@@ -0,0 +1,5 @@
+const lienzo = document.querySelector('#lienzo');
+const ctx = lienzo.getContext('2d');
+//(x,y,ancho,alto)
+ctx.fillStyle = 'red'; //colorear = Style
+ctx.fillRect(0,0,20,20); //dibujar = Rect
